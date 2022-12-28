671449e6593ca98b2355106f08116f4c7663bb6f
Moise-MM <moisemutangala39@gmail.com>
2022-12-28 21:30:53 +0000

    add uploadImage method that allows to store image from ckeditor in public folder

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -6,8 +6,35 @@ use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
+    /**
+     * show form
+     *
+     */
     public function index()
     {
         return view('products.home');
+    }
+
+
+    public function uploadImage(Request $request)
+    {
+        //check if an image file is uploaded
+        if($request->hasFile('upload'))
+        {
+            $originName = $request->file('upload')->getClientOriginalName();
+            $fileName = pathinfo($originName, PATHINFO_FILENAME);
+            $extension = $request->file('upload')->getClientOriginalExtension();
+            $fileName = $fileName.'_'.time().'.'.$extension;
+
+            $request->file('upload')->move(public_path('images'), $fileName);
+            $CKEditorFuncNum = $request->input('CKEditorFuncNum');
+            $url = asset('images/'.$fileName); 
+            $msg = 'Image successfully uploaded'; 
+            $response = "<script>window.parent.CKEDITOR.tools.callFunction($CKEditorFuncNum, '$url', '$msg')</script>";
+               
+            @header('Content-type: text/html; charset=utf-8'); 
+            echo $response;
+            
+        }
     }
 }
