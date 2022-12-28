eb84116f65d4a81debc1fd55b2d3170892442842
Moise-MM <moisemutangala39@gmail.com>
2022-12-28 21:21:12 +0000

    add index method inside ProductController that allows to show form and ckeditor

---
+++ resources/views/products/home.blade.php
@@ -0,0 +1,41 @@
+@extends('layout')
+
+@section('content')
+    
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <h1 class="text-center">Upload Image Using CKeditor</h1>
+                <form method="POST" action="" enctype="multipart/form-data">
+                    @csrf
+                    <div class="form-group">
+                        <label>Title</label>
+                        <input type="text" name="name" class="form-control">
+                    </div>
+                    <div class="form-group">
+                        <label>Description</label>
+                        <textarea class="form-control" id="summary-ckeditor" name="description"></textarea>
+                    </div>
+                    <div class="form-group">
+                        <label>Price</label>
+                        <input type="text" name="price" class="form-control">
+                    </div>
+                    <div class="form-group">
+                        <input type="submit" value="Submit" class="btn btn-primary mt-3">
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+
+
+    <script src="https://cdn.jsdelivr.net/npm/jquery@3.6.3/dist/jquery.min.js"></script>
+    <script src="https://cdn.ckeditor.com/4.20.1/standard/ckeditor.js"></script>
+    <script type="text/javascript">
+        CKEDITOR.replace('summary-ckeditor', {
+            filebrowserUploadUrl: "{{route('upload', ['_token' => csrf_token() ])}}",
+            filebrowserUploadMethod: 'form'
+        });
+    </script>
+
+@endsection
